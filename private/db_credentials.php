bceaeac38e4e41bfc878dad0d17dfc5dd22b7eb9
jesterfs <63027978+jesterfs@users.noreply.github.com>
2022-08-25 22:05:05 -0700

    Update db_credentials.php

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,13 +1,11 @@
 <?php
+//This Repo is only for portfolio purposes and will not function as is.
 
-define("DB_SERVER", "us-cdbr-east-06.cleardb.net");
-define("DB_USER", "b13ae729b081d2");
-define("DB_PASS", "f11609c9");
-define("DB_NAME", "heroku_14f6e73d6998a0a");
+define("DB_SERVER", "REDACTED");
+define("DB_USER", "REDACTED");
+define("DB_PASS", "REDACTED");
+define("DB_NAME", "REDACTED");
 
-//define("DB_SERVER", "localhost:3306");
-//define("DB_USER", "root");
-//define("DB_PASS", "root");
-//define("DB_NAME", "jester_port");
+
 
 ?>
